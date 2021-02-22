000fc745ab7518333f4a472daade8d22d10779e0
Roberto Piancatelli <piancatellirob@gmail.com>
2021-02-22 17:33:21 +0000

    added island model

---
+++ db/migrate/20210222172909_create_islands.rb
@@ -0,0 +1,15 @@
+class CreateIslands < ActiveRecord::Migration[6.0]
+  def change
+    create_table :islands do |t|
+      t.string :name
+      t.string :location
+      t.integer :price_per_night
+      t.string :description
+      t.integer :max_guests
+      t.references :user, null: false, foreign_key: true
+      t.references :bookings, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
