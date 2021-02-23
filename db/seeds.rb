c3be4eb6f7b6fcd4a0d3e6bc9daa983234a1fa7a
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-23 14:03:04 +0000

    adding seed file, faker gem & image_url to islands db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "faker"
+
+
+
+5.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password"
+    )
+
+  rand(1..5).times do
+    island = Island.create(
+      name: Faker::Ancient.titan,
+      location: Faker::Nation.nationality,
+      price_per_night: 100,
+      description: Faker::Lorem.sentences(number: 4),
+      max_guests: 4,
+      user_id: user.id,
+      image_url: "https://www.meditainment.com/images/dmImage/SourceImage/island-paradise.jpg"
+      )
+  end
+end
+
+
