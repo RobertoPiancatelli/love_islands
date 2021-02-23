5fabe85462b222fcd8ba2b09df46d40e9f600aa5
Roberto Piancatelli <piancatellirob@gmail.com>
2021-02-23 16:20:41 +0000

    committed

---
+++ app/controllers/islands_controller.rb
@@ -0,0 +1,55 @@
+class IslandsController < ApplicationController
+  # INDEX
+  def index
+    @islands = Island.all
+  end
+
+  # NEW
+  def new
+    @island = Island.new
+  end
+
+  # SHOW
+  def show
+    @island = Island.find(params[:id])
+  end
+
+  # CREATE
+  def create
+    @island = Island.new(island_params)
+    @island.user = current_user
+    if @island.save
+      redirect_to island_path(@island)
+    else
+      render :new
+    end
+  end
+
+  # DESTROY
+  def destroy
+    @island = Island.find(params[:id])
+    @island.destroy
+    redirect_to islands_path
+  end
+
+  # EDIT
+  def edit
+    @island = Island.find(params[:id])
+  end
+
+  # UPDATE
+  def update
+    @island = Island.find(params[:id])
+    if @island.update(island_params)
+      redirect_to island_path(@island)
+    else
+      render :edit
+    end
+  end
+end
+  # PRIVATE PARAMS
+  private
+
+  def island_params
+    params.require(:island).permit(:name, :location, :price_per_night, :description, :max_guests)
+  end
