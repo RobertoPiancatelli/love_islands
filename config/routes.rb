fb43e16686219e6d5bbcb73bfc421ad3ecc57fc3
Roberto Piancatelli <piancatellirob@gmail.com>
2021-02-24 11:32:57 +0000

    Added reviews on booking

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :islands do
-    resources :bookings
-     resources :reviews, only: [:new]
+    resources :bookings, only: [:new, :create, :index]
+  end
+  resources :bookings, only: [:show] do
+    resources :reviews, only: [:new, :create]
   end
 end
