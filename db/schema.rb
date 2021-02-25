6b3baefd5ec8d000cecfc88a580a45be26ac1c41
Lukas Musilek <lukas.musilek@gmail.com>
2021-02-25 12:43:03 +0000

    fixed all hash names for price_per_nigth to price_cents to use it with money rails

--- db/schema.rb
+++ db/schema.rb
@@ -58,9 +58,9 @@ ActiveRecord::Schema.define(version: 2021_02_25_112334) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_url"
-    t.integer "price_cents", default: 0, null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
