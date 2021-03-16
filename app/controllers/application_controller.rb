aaf9e50b2ebdbaa24d75c0039e2899318d7a784b
Roberto Piancatelli <piancatellirob@gmail.com>
2021-03-16 18:56:08 +0000

    added url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["https://love-islands.herokuapp.com/"] || "localhost:3000" }
+    { host: ENV["https://love-islands.herokuapp.com"] || "localhost:3000" }
   end
 end
